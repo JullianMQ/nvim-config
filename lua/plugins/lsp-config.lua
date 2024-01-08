ac19bd9fcaa9bfedfd81177a763cf0961ec4c7a9
JullianMQ <jullianmq@gmail.com>
2024-01-08 10:43:53 +0800

    added auto completion somewhat

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,15 +2,18 @@ return
     {
         {
             "williamboman/mason.nvim",
+            lazy = false,
             config = function()
                 require("mason").setup()
             end
         },
         {
             "williamboman/mason-lspconfig.nvim",
+            lazy = false,
             config = function()
                 require("mason-lspconfig").setup
                 {
+                    auto_install = true,
                     ensure_installed =
                         {
                             "lua_ls",
@@ -40,29 +43,53 @@ return
         },
         {
             "neovim/nvim-lspconfig",
+            lazy = false,
             config = function()
+                local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
                 local lspconfig = require('lspconfig')
-                lspconfig.lua_ls.setup({})
-                lspconfig.bashls.setup({})
-                lspconfig.clangd.setup({})
-                lspconfig.emmet_ls.setup({})
-                lspconfig.emmet_language_server.setup({})
-                lspconfig.cssls.setup({})
-                lspconfig.cssmodules_ls.setup({})
-                lspconfig.tailwindcss.setup({})
-                lspconfig.unocss.setup({})
-                lspconfig.eslint.setup({})
-                lspconfig.svelte.setup({})
-                lspconfig.html.setup({})
-                lspconfig.htmx.setup({})
-                lspconfig.sqlls.setup({})
-                lspconfig.jdtls.setup({})
-                lspconfig.tsserver.setup({})
-                lspconfig.vtsls.setup({})
-                lspconfig.intelephense.setup({})
-                lspconfig.pyright.setup({})
-                -- lspconfig.pylsp.setup({})
-                lspconfig.vimls.setup({})
+                lspconfig.lua_ls.setup({
+                capabilities = capabilities})
+                lspconfig.bashls.setup({
+                capabilities = capabilities})
+                lspconfig.clangd.setup({
+                capabilities = capabilities})
+                lspconfig.emmet_ls.setup({
+                capabilities = capabilities})
+                lspconfig.emmet_language_server.setup({
+                capabilities = capabilities})
+                lspconfig.cssls.setup({
+                capabilities = capabilities})
+                lspconfig.cssmodules_ls.setup({
+                capabilities = capabilities})
+                lspconfig.tailwindcss.setup({
+                capabilities = capabilities})
+                lspconfig.unocss.setup({
+                capabilities = capabilities})
+                lspconfig.eslint.setup({
+                capabilities = capabilities})
+                lspconfig.svelte.setup({
+                capabilities = capabilities})
+                lspconfig.html.setup({
+                capabilities = capabilities})
+                lspconfig.htmx.setup({
+                capabilities = capabilities})
+                lspconfig.sqlls.setup({
+                capabilities = capabilities})
+                lspconfig.jdtls.setup({
+                capabilities = capabilities})
+                lspconfig.tsserver.setup({
+                capabilities = capabilities})
+                lspconfig.vtsls.setup({
+                capabilities = capabilities})
+                lspconfig.intelephense.setup({
+                capabilities = capabilities})
+                lspconfig.pyright.setup({
+                capabilities = capabilities})
+                -- lspconfig.pylsp.setup({
+                -- capabilities = capabilities})
+                lspconfig.vimls.setup({
+                capabilities = capabilities})
 
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
