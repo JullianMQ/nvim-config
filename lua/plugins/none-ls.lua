915b46dd5ef43e9d767c808b17d5a3c6efde0024
JullianMQ <jullianmq@gmail.com>
2024-01-19 19:35:07 +0800

    small config change

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.black,
-                -- null_ls.builtins.formatting.ast_grep,
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.djlint,
                 null_ls.builtins.formatting.google_java_format,
@@ -20,13 +19,10 @@ return {
                 null_ls.builtins.diagnostics.djlint,
                 null_ls.builtins.diagnostics.curlylint,
                 null_ls.builtins.diagnostics.eslint_d,
-                -- null_ls.builtins.diagnostics.htmlhint,
                 null_ls.builtins.diagnostics.phpcs,
                 null_ls.builtins.diagnostics.pylama,
-                -- null_ls.builtins.diagnostics.sonarlint-language-server,
                 null_ls.builtins.diagnostics.sqlfluff,
                 null_ls.builtins.diagnostics.stylelint,
-                null_ls.builtins.diagnostics.typos,
             },
         })
 
