4d5d81827eb39464aeda6b2e32d729344b8be3de
Jullian Quiambao <jullianmq@gmail.com>
2025-07-26 16:14:16 +0800

    added other keymaps for substitute

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -269,6 +269,7 @@ return {
 			require("supermaven-nvim").setup({})
 		end,
 	},
+	 
 	-- Substitute NVIM
 	{
 		"gbprod/substitute.nvim",
@@ -278,6 +279,10 @@ return {
 			vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
 			vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
 			vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
+			vim.keymap.set("n", "sx", require('substitute.exchange').operator, { noremap = true })
+			vim.keymap.set("n", "sxx", require('substitute.exchange').line, { noremap = true })
+			vim.keymap.set("x", "X", require('substitute.exchange').visual, { noremap = true })
+			vim.keymap.set("n", "sxc", require('substitute.exchange').cancel, { noremap = true })
 		end
 	},
 }
